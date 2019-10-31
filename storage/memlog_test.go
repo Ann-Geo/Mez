ccb4488487753f5efa2a7716cb56516ba6e2911f
Anjus-George <ageorg28@uncc.edu>
2019-10-31 18:21:03 -0400

    added TestAppendPartiallyWritten

--- storage/memlog_test.go
+++ storage/memlog_test.go
@@ -1,12 +1,69 @@
 package storage
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 	"time"
 )
 
+//***************************Helper functions start*****************************************
+
+/*To return list of absolute paths of files in a directory
+Input - directory path
+Output- list of filepaths
+*/
+func walkAllFilesInDir(dir string) []string {
+	fileList := make([]string, 0)
+	_ = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// check if it is a regular file (not dir)
+		if info.Mode().IsRegular() {
+			fileList = append(fileList, path)
+
+		}
+		return err
+	})
+
+	return fileList
+}
+
+/*To compare equality of timestamp slices (Appended and Read)
+and to compare equality of image size slices (Appended and Read)
+Input - Appended and Read slices
+Output- true or false boolean
+*/
+func sliceEquality(tsAppended, tsRead []time.Time, imSizeAppended, imSizeRead []int) bool {
+	if len(tsAppended) != len(tsRead) {
+		return false
+	}
+
+	if len(imSizeAppended) != len(imSizeRead) {
+		return false
+	}
+
+	for i := 0; i < len(tsAppended); i++ {
+		if tsAppended[i] != tsRead[i] {
+			return false
+		}
+		if imSizeAppended[i] != imSizeRead[i] {
+			return false
+		}
+	}
+	return true
+}
+
+//**********************************Helper functions end***********************************
+
+//***********************************Storage tests start************************************
+/*
+First test - tests storage Append and Read
+*/
 func TestAppendRead(t *testing.T) {
 	var (
 		imBuf          []byte
@@ -83,69 +140,93 @@ func TestAppendRead(t *testing.T) {
 }
 
 /*
-// Test concurrent append and reads
-func TestConcurrentAppendRead(t *testing.T) {
-	// Note: Issues with printing from go threads in test. Currently printing only from main thread
+Tests storage capability to store same sized images
+Tests only storage Append
+Test for partially written, fully written and overwritten logs
+To run this test use command,
+> FP=/path/to/images/ NO_IM=1000 LS=2000 SS=100 go test -v -run="TestAppendPartiallyWritten"
+FP is path to images, NO_IM is no of images to be inserted
+LS is log size and SS is segment size
+Frame rate should be specified in code
+*/
+func TestAppendPartiallyWritten(t *testing.T) {
+
+	imageFilesPath := os.Getenv("FP")
+	noImages := os.Getenv("NO_IM")
+	noImagesInsert, _ := strconv.Atoi(noImages)
+	logSz := os.Getenv("LS")
+	logSize, _ := strconv.ParseUint(logSz, 10, 64)
+	segSz := os.Getenv("SS")
+	segSize, _ := strconv.ParseUint(segSz, 10, 64)
+
+	//frame rate should be specified after inserting the timestamp
 	var (
-		imBuf          []byte
-		imageFilesPath = "../test_images"
-		err            error
+		imBuf []byte
+		err   error
 	)
 
-	memlog := NewMemLog(2, 8)
-	start := time.Now()
+	//new memlog
+	memlog := NewMemLog(segSize, logSize)
 
-	// Read image file names from directory
-	var files []string
-	absImageFilePath, _ := filepath.Abs(imageFilesPath)
-	err = filepath.Walk(absImageFilePath, func(path string, info os.FileInfo, err error) error {
-		files = append(files, path)
-		return nil
-	})
+	//Obtain filenames in the directory path given
+	fileList := walkAllFilesInDir(imageFilesPath)
+
+	//read first file to buffer (conversion to bytes)
+	imBuf, err = ioutil.ReadFile(fileList[0])
 	if err != nil {
+		t.Errorf("Cannot read image file %v\n", err)
 		panic(err)
+
 	}
 
-	// Load a single image file to buffer
-	fd, err := os.Open(files[1])
-	if err != nil {
-		panic(err)
-	}
-	defer fd.Close()
-	fi, err := fd.Stat()
-	imBuf = make([]byte, fi.Size())
-	_, err = fd.Read(imBuf)
-	if err != nil {
-		t.Errorf("errored while copying from file to buf %v", err)
+	//slices to store timestamps and image sizes appended
+	tsAppended := make([]time.Time, 0)
+	imSizeAppended := make([]int, 0)
+
+	//Append image sizes to the slice
+	for i := 0; i < noImagesInsert; i++ {
+		imSizeAppended = append(imSizeAppended, len(imBuf))
 	}
 
-	// Append to log in an infinite loop
-	go func() {
-		for {
-			ts := time.Now()
-			memlog.Append(Image(imBuf), ts)
-			time.Sleep(100 * time.Millisecond)
-			//	sz, pos, tins := memlog.AppendStats()
+	// Append to log
+	for i := 0; i < noImagesInsert; i++ {
+		ts := time.Now()
+		//fmt.Println(ts)
+		tsAppended = append(tsAppended, ts)
+		memlog.Append(Image(imBuf), ts)
+		//Specify frame rate here
+		time.Sleep(33 * time.Millisecond)
+		sz, pos, tins := memlog.AppendStats()
+		//imSizeAppended = append(imSizeAppended, sz)
+		t.Logf("Image of size %d inserted in log at position %d at time %v\n", sz, pos, tins)
 
+	}
+
+	//verifying the timestamps and image sizes directly from log
+	//count variable to keep track of number of elements in log
+	var count int
+	//slices to read timestamps and image sizes
+	tsRead := make([]time.Time, 0)
+	imSizeRead := make([]int, 0)
+	var i uint64
+	var j uint64
+	for i = 0; i < logSize/segSize; i++ {
+		for j = 0; j < segSize; j++ {
+			if count == noImagesInsert {
+				break
+			}
+
+			tsRead = append(tsRead, memlog.tsmemlog.tslog[i].ts[j])
+			imSizeRead = append(imSizeRead, len(memlog.immemlog.imlog[i].im[j]))
+			count = count + 1
 		}
-	}()
 
-	// Read from log
-	time.Sleep(1 * time.Second)
-	stop := time.Now()
-	imts := make(chan ImageTimestamp, 200*1024)
-	errc := make(chan error)
+	}
 
-	go memlog.Read(imts, start, stop, errc)
-
-	err = <-errc
-	if err != nil {
-		t.Log(err)
-	} else {
-		for imts := range imts {
-			t.Logf("Size of image is %d and timestamp is %v \n", len(imts.Im), imts.Ts)
-		}
+	if !(sliceEquality(tsAppended, tsRead, imSizeAppended, imSizeRead)) {
+		t.Errorf("Memlog append failed")
 	}
 
 }
-*/
+
+//*****************************Storage tests end***************************************
