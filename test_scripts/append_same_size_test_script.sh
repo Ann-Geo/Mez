f8dfa617b431222f091da84f553c5491e000a9e8
Anjus-George <ageorg28@uncc.edu>
2019-11-01 18:36:07 -0400

    added script to test

---
+++ test_scripts/append_same_size_test_script.sh
@@ -0,0 +1,141 @@
+cd ../storage
+
+echo TestSameSizeImage at 30FPS frame rate
+
+echo FOR 2.1M, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/2.1M/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/2.1M/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/2.1M/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 1.5M, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.5M/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.5M/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.5M/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 1.2M, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.2M/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.2M/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.2M/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 1.0M, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.0M/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.0M/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/1.0M/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 800K, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/800K/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/800K/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/800K/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 500K, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/500K/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/500K/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/500K/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 200K, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/200K/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/200K/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/200K/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 100K, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/100K/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/100K/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/100K/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 50K, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/50K/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/50K/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/50K/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo FOR 10K, 30fps, 24fps and 15fps
+echo TestFullyWritten
+echo logsize - 1000, number of images - 1000
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/10K/ NO_IM=1000 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestPartiallyWritten
+echo logsize - 1000, number of images - 500
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/10K/ NO_IM=500 LS=10 SS=100 go test -v -run="TestAppendSameSizeImage"
+echo TestOverWritten
+echo logsize - 300, number of images - 545
+FP=/home/research/goworkspace/src/github.com/arun-ravindran/test_images/10K/ NO_IM=545 LS=10 SS=30 go test -v -run="TestAppendOverWrittenLog"
+
+
+
+echo --------------------------------------------------------------------
+
+
+
+
+
+
